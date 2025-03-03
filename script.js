4155ff9d76d80017d579d89a20c568696971a144
laksh-ghai <laksh.rce@gmail.com>
2025-03-02 19:43:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 async function getStockData() {
-     alert("1")
+
     let ticker = document.getElementById("stockTicker").value.toUpperCase().trim();
     
     if (!ticker) {
         alert("Please enter a stock ticker!");
         return;
     }
- alert("2")
+
     const apiKey1 = "8a5a6d83aemshc1932fb3028c609p117effjsn769029e7fd22"; // Replace with your actual API key
- alert("3")
+
     // Fetch stock price
     const priceUrl = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes?symbols=${ticker}&region=US`;
     const statsUrl = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v4/get-statistics?symbols=${ticker}&region=US`;
- alert("4")
+
     const options = {
         method: "GET",
         headers: {
@@ -20,15 +20,14 @@ async function getStockData() {
             "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
         }
     };
- alert("5")
+
     try {
         // Fetch stock price
-        alert("6")
+
         const priceResponse = await fetch(priceUrl, options);
         const priceData = await priceResponse.json();
-alert("7")
+
         if (priceData.quoteResponse && priceData.quoteResponse.result.length > 0) {
-            alert("3")
             let stock = priceData.quoteResponse.result[0];
             let price = stock.regularMarketPrice;
             let change = stock.regularMarketChangePercent.toFixed(2);
@@ -45,15 +44,12 @@ alert("7")
                 <b>50-Day High </b> $${fiftyHigh} <br>
                 <b>50-Day Low </b> $${fiftyLow} <br>             
                 `;
-            alert("4")
         } else {
             document.getElementById("result").innerHTML = "❌ Invalid stock ticker.";
             return;
         }
 
-        alert("5")
     } catch (error) {
-        alert("6")
         console.error("Error fetching stock data:", error);
         document.getElementById("result").innerHTML = "❌ Could not fetch stock data.";
         document.getElementById("stats").innerHTML = "";
