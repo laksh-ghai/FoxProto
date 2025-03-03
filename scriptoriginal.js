45a9245cc8fe440e293247fba7e9ee8831a82841
laksh-ghai <laksh.rce@gmail.com>
2025-03-02 19:21:12 -0600

    Update scriptoriginal.js

--- scriptoriginal.js
+++ scriptoriginal.js
@@ -52,7 +52,7 @@ class FoxFinancialAI {
     async getRealStockPrice(ticker) {
         const apiKey = "8a5a6d83aemshc1932fb3028c609p117effjsn769029e7fd22'";  // Replace with your actual RapidAPI key
         const url = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes?symbols=${ticker}&region=US`;
-alert("3")
+alert("ORI_3")
         try {
             const response = await fetch(url, {
                 method: "GET",
@@ -61,7 +61,7 @@ alert("3")
                     "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
                 }
             });
-alert("4")
+alert("ORI_4")
             if (!response.ok) {
                 throw new Error(`Stock API Error: ${response.status} ${response.statusText}`);
             }
